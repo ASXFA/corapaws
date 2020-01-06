e586f25d7f5a5f2171a07407f8bfc40465656fa2
ASXFA <fachri.agustian12@gmail.com>
2020-01-06 15:42:02 +0700

    menambahkan home dan memperbaiki struktur coding codeigniter

---
+++ application/views/user/layout/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title><?= $judul ?></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" type="image/png" href="<?= base_url();?>assets/logo/logo_p1.png">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+
+    <link rel="stylesheet" href="<?= base_url();?>assets/user/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url();?>assets/user/css/animate.css">
+    
+    <link rel="stylesheet" href="<?= base_url();?>assets/user/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?= base_url();?>assets/user/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="<?= base_url();?>assets/user/css/magnific-popup.css">
+
+    <link rel="stylesheet" href="<?= base_url();?>assets/user/css/aos.css">
+
+    <link rel="stylesheet" href="<?= base_url();?>assets/user/css/ionicons.min.css">
+
+    <link rel="stylesheet" href="<?= base_url();?>assets/user/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="<?= base_url();?>assets/user/css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="<?= base_url();?>assets/user/css/flaticon.css">
+    <link rel="stylesheet" href="<?= base_url();?>assets/user/css/icomoon.css">
+    <link rel="stylesheet" href="<?= base_url();?>assets/user/css/style.css">
+  </head>
+  <body class="goto-here">

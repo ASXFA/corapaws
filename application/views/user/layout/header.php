adac6ecbdd2be3d8062b557987e094446bb96494
unknown <ndrayana2013@gmail.com>
2020-01-15 20:25:34 +0800

    tos, privacy policy, done

--- application/views/user/layout/header.php
+++ application/views/user/layout/header.php
@@ -20,7 +20,7 @@
 
     <link rel="stylesheet" href="<?= base_url();?>assets/user/css/bootstrap-datepicker.css">
     <link rel="stylesheet" href="<?= base_url();?>assets/user/css/jquery.timepicker.css">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <link rel="stylesheet" href="<?= base_url();?>assets/user/css/flaticon.css">
     <link rel="stylesheet" href="<?= base_url();?>assets/user/css/icomoon.css">
